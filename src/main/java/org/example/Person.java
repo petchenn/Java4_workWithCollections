af328ec1ffac73a7daa56c8a4d3a980ed2edeb93
petchenn <alya030305z@gmail.com>
2025-05-10 14:27:45 +0300

    Исправление конструктора и метода toString() в Person

--- src/main/java/org/example/Person.java
+++ src/main/java/org/example/Person.java
@@ -1,6 +1,8 @@
 package org.example;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
 public class Person {
     private int ID;
@@ -9,6 +11,28 @@ public class Person {
     private Division division;
     private double salary;
     private LocalDate dateOfBirn;
+
+    public Person(String[] data) {
+        ID = Integer.parseInt(data[0]);
+        name = data[1];
+        gender = data[2];
+        dateOfBirn = dateFromString(data[3]);
+        division = new Division(data[4]);
+        salary = Integer.parseInt(data[5]);
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "ID=" + ID +
+                ", name='" + name + '\'' +
+                ", gender='" + gender + '\'' +
+                ", division=" + division.getName() +
+                ", divisionID=" + division.getId() +
+                ", salary=" + salary +
+                ", birth=" + dateOfBirn +
+                '}';
+    }
 
     public int getID() {
         return ID;
@@ -56,5 +80,16 @@ public class Person {
 
     public void setDateOfBirn(LocalDate dateOfBirn) {
         this.dateOfBirn = dateOfBirn;
+    }
+
+    private LocalDate dateFromString(String datum) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
+        try {
+            return LocalDate.parse(datum, formatter);
+        } catch (DateTimeParseException e) {
+            System.err.println("Ошибка при парсинге даты: " + e.getMessage());
+            return null;
+        }
     }
 }
